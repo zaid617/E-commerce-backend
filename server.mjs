8abcd6d675c77151d8d4010850cc709ee6c243f4
Zaid_ZiD <zid6177@gmail.com>
2023-09-02 03:53:31 +0500

    initialize backend

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+
+const PORT = process.env.PORT || 5001;
+const app = express();
+
+app.use("/", (req, res)=>{
+
+    res.status(200).send("hello world!");
+
+})
+
+app.listen(PORT , ()=>{
+    console.log("listening on port: " + PORT);
+})
