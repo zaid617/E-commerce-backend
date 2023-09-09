d1a46f1c00fa4c01ce9ebfb9b193e979d5cdc625
Zaid_ZiD <zid6177@gmail.com>
2023-09-09 15:12:19 +0500

    path define

--- server.mjs
+++ server.mjs
@@ -7,13 +7,14 @@ import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
 
 const PORT = process.env.PORT || 5001;
 
 const app = express();
 
 const corsOptions = {
-  origin: ["https://dnk-shop.netlify.app", "http://localhost:3000" , "*"], // Replace with your allowed origin(s)
+  origin: ["https://dnk-shop.netlify.app", "http://localhost:3000"], // Replace with your allowed origin(s)
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 204, // No content response for preflight requests
   credentials: true,
@@ -27,6 +28,12 @@ app.use("/api/v1", authApis);
 app.use("/api/v1", products);
 app.use("/api/v1", cart);
 app.use("/api/v1", order);
+
+const __dirname = path.resolve();
+
+app.use("/", express.static(path.join(__dirname, "./build")));
+app.use("*", express.static(path.join(__dirname, "./build")));
+
 
 app.listen(PORT, () => {
   console.log("listening on port: " + PORT);
