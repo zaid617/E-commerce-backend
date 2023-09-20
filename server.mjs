2f8e54e4527205ea834ad038630a5c4e390e21bf
Zaid_ZiD <zid6177@gmail.com>
2023-09-21 01:21:13 +0500

    middleware added

--- server.mjs
+++ server.mjs
@@ -9,33 +9,78 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import path from "path";
 
-const PORT = process.env.PORT || 5001;
+
 const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(bodyParser())
+app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser());
 
 app.use(cors({
-  origin: ["https://dnk-shop.netlify.app" , "http://localhost:3000" , "*"]
+
+
+  origin: [
+    "https://dnk-shop.netlify.app",
+    "http://localhost:3000",
+    "*"
+  ]
   ,
+
     credentials: true,
+
 })
 );
 
 
 app.use("/api/v1", authApis);
 
-// middleware will be added here //
+
+app.use("/api/v1", (req, res, next) => {
+
+  if (!req?.cookies?.Token) {
+    res.status(401).send({
+      message: "include http-only credentials with every request",
+    });
+    return;
+  }
+  
+  jwt.verify(req.cookies.Token, SECRET, (err, decodedData) => {
+    if (!err) {
+
+      const nowDate = new Date().getTime() / 1000;
+
+      if (decodedData.exp < nowDate) {
+        res.status(401).send({ message: "token expired" });
+        res.cookie("Token", " ", {
+          maxAge: 1,
+          httpOnly: true,
+          sameSite: "none",
+          secure: true,
+        });
+      } else {
+        console.log("token approved");
+
+        req.body.token = decodedData;
+        next();
+      }
+    } else {
+      res.status(401).send("invalid token");
+    }
+  });
+});
+
 
 app.use("/api/v1", products);
 app.use("/api/v1", cart);
 app.use("/api/v1", order);
+
 
 const __dirname = path.resolve();
 
 app.use("/", express.static(path.join(__dirname, "./build")));
 app.use("*", express.static(path.join(__dirname, "./build")));
 
-
-app.listen(PORT, () => {
-  console.log("listening on port: " + PORT);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+})
